fed874669489ce538f43aac86cc795141cee91b9
Rafael Fernandes <rafaelbenzi@estudante.ufscar.br>
2025-02-12 11:17:14 +0100

    TP2 integration

--- TP2/agent.py
+++ TP2/agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def gen_traj(env, T=5):
     ''' Generate a path with associated observations.
@@ -61,7 +62,7 @@ class Agent:
 
         for t in range(T):
             if t == 0:
-                first_s = [i[-1] for i in np.argwhere(env.P_1)]
+                first_s = np.flatnonzero(env.P_1)
                 all_paths.extend([[s] for s in first_s])
 
             elif t == 1:
@@ -93,7 +94,7 @@ class Agent:
         '''
         Provides full conditional distribution P(SSS | ooo) where SSS and ooo are sequences of length T.
         $$
-            P( Y_1,\ldots,Y_T | o_1,\ldots,o_T )
+            P( Y_1,\ldots,Y_T | o_1,\ldots, o_T )
         $$
 
         Parameters
@@ -118,11 +119,10 @@ class Agent:
             the string representation of np.array([1,2,3,4],dtype=int) should be '1 2 3 4'. 
         '''        
         prob = {}
+        p = {}
         env = self.env
         
         if M == -1:
-            corners = [0, 5, 24, 29]
-            sides = [1, 2, 3, 4, 6, 11, 12, 17, 18, 23, 25, 26, 27, 28]
 
             for i, o in enumerate(ooo):
                 o = np.array(o, dtype=int)
@@ -131,24 +131,61 @@ class Agent:
 
                 if i == 0:
                     for s in all_paths:
-                        if len(s) == 1:
-                            prob[str(np.array(s))[1:-1]] = round(float(env.P_1[s]), 5)
+                        prob[str(np.array(s))[1:-1]] = round(float(env.P_1[s]), 5)
                 else:
                     for s in all_paths:
                         current_key = str(np.array(s))[1:-1]
                         path_key = str(np.array(s[:-1]))[1:-1]
-                        if s[-2] in corners:
-                            prob_a_priori = prob[path_key] / 2
-                        elif s[-2] in sides:
-                            prob_a_priori = prob[path_key] / 3
-                        else:
-                            prob_a_priori = prob[path_key] / 4
-                        prob[current_key] = round(prob_a_priori * env.P_O[s[-1], 0, o[0]] * env.P_O[s[-1], 1, o[1]], 5)
+                        # Prior probability = probability of path to tile * probability of that tile given the path
+                        prior_prob = prob[path_key] * max(env.P_S[s[-2]])
+                        prob[current_key] = prior_prob * env.P_O[s[-1], 0, o[0]] * env.P_O[s[-1], 1, o[1]]
                         prob_noise += prob[current_key]
 
             # Filter the paths and probabilities added in the last loop
             p = {k: v/prob_noise for k, v in prob.items() if len(k.split()) == len(all_paths[0])}
-                            
+
+        elif M > 0:
+            for _ in range(M):
+                for i, o in enumerate(ooo):
+                    o = np.array(o, dtype=int)
+
+                    if i == 0:
+                        possible_paths = np.flatnonzero(env.P_1)
+                        chosen_path = int(np.random.choice(possible_paths))
+
+                    elif i == 1:
+                        possible_paths = self.s_tree(2)
+                        possible_tiles = np.array([path[-1] for path in possible_paths])
+                        probability_tile = np.zeros(len(possible_tiles))
+
+                        for j, s in enumerate(possible_tiles):
+                            probability_tile[j] = env.P_O[s, 0, o[0]] * env.P_O[s, 1, o[1]]
+                        
+                        probability_tile = probability_tile/sum(probability_tile)
+                        chosen_path = possible_paths[np.random.choice(range(len(possible_paths)), p=probability_tile)]
+
+                    else:
+                        possible_tiles = np.flatnonzero(env.P_S[chosen_path[-1]])
+                        possible_paths = [list(chosen_path) for _ in range(len(possible_tiles))]
+                        probability_tile = np.zeros(len(possible_tiles))
+
+                        for j, s in enumerate(possible_tiles):
+                            probability_tile[j] = round(env.P_O[s, 0, o[0]] * env.P_O[s, 1, o[1]], 5)
+                            possible_paths[j].append(s)
+                        
+                        probability_tile = probability_tile/sum(probability_tile)
+                        chosen_path = possible_paths[np.random.choice(range(len(possible_paths)), p=probability_tile)]
+                
+                if str(np.array(chosen_path))[1:-1] in prob.keys():
+                    prob[str(np.array(chosen_path))[1:-1]] += probability_tile[possible_paths.index(chosen_path)]
+                else:
+                    prob[str(np.array(chosen_path))[1:-1]] = probability_tile[possible_paths.index(chosen_path)]
+            
+            p = {k: v/sum(prob.values()) for k, v in prob.items()}
+        
+        else:
+            raise ValueError("'M' parameter can only be greater than 0 or -1")
+
         return p
 
         
@@ -188,7 +225,7 @@ class Agent:
 
         for path in prob_paths:
             P[int(path.split()[-1])] += p[path]
-        
+
         return P
 
     def Q(self, o): 
@@ -208,9 +245,17 @@ class Agent:
                 such that Q[a] is the value (expected reward) of action a.
 
         '''
-        Q = np.zeros(self.env.n_states)
-        # TODO 
-        return Q
+        Q = []
+        P_S = self.P_S(o)
+        max_probability = max(P_S)
+        
+        for prob in P_S:
+            if prob == max_probability:
+                Q.append(1)
+            else:
+                Q.append(0)
+
+        return np.array(Q)
 
     def act(self, obs): 
         '''
@@ -229,7 +274,48 @@ class Agent:
             the chosen action a
         '''
 
-        a = -1
-        # TODO 
+        P_S = self.P_S(obs)
+        max_probability = max(P_S)
+        most_probable_tile = np.flatnonzero(P_S == max_probability)
+        a = np.random.choice(most_probable_tile)
+
         return a
 
+if __name__ == '__main__':
+    
+    from environment import Environment
+
+    # Instantiate the environment
+    G = np.array([[1,3,0,2,4,1],
+                [2,1,0,3,0,3],
+                [4,0,3,0,2,0],
+                [3,1,2,3,0,4],
+                [2,0,0,0,1,1]])
+    # The fps flag allows for false positives (make sure you have the recent version of envioronment.py)
+    env = Environment(G,fps=True)
+
+    # Generate a trajectory
+    ooo, sss = gen_traj(env,10)
+
+    # Instantiate your agent
+    agent = Agent(env)
+
+    # Use your new implementation, by specifying M>0
+    P_joint = agent.P_traj(ooo, M=100)
+    print(sss)
+    print(ooo)
+    try:
+        print(P_joint[str(sss)[1:-1]])
+    except:
+        pass
+
+    # Create fig
+    plt.figure()
+    plt.bar(list(P_joint.keys()), list(P_joint.values()))
+    plt.xlabel(r"$\mathbf{s}$")
+    plt.ylabel(r"$p(\mathbf{s}|\mathbf{o})$")
+    plt.xticks(rotation=45, ha='right')
+
+    # Show the plot
+    plt.tight_layout()
+    plt.show()
